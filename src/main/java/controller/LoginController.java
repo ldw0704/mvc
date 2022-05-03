a3aef0ccc67b1092c51771fb026ed6c17c62d36b
이동욱 <ldw0704@gmail.com>
2022-05-03 09:16:12 +0900

    테스트용

--- src/main/java/controller/LoginController.java
+++ src/main/java/controller/LoginController.java
@@ -21,7 +21,7 @@ import service.LoginServiceImpl;
 public class LoginController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
-    /**
+    /*
      * @see HttpServlet#HttpServlet()
      */
     public LoginController() {
